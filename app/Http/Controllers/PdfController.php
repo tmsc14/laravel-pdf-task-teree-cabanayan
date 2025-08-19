c7d9a0eed12ecc3f41563ad6a6ff20df9efb404e
teree <ctereemari@gmail.com>
2025-08-20 00:57:01 +0800

    added another function for file fl309

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,20 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class PdfController extends Controller
 {
-    // Preview HTML version in browser
     public function view()
     {
         return view('pdf.test');
     }
 
-    // Download PDF
     public function download()
     {
         $pdf = Pdf::loadView('pdf.test');
         return $pdf->download('test.pdf');
+    }
+    public function generateFL309(Request $request)
+    {
+        
+        $pdf = PDF::loadView('pdf.fl309');
+        $pdf->setPaper('letter', 'portrait');
+
+        return $pdf->stream('FL-309.pdf');
     }
 }
